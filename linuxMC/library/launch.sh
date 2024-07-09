8b8bc1b07df77fe67eab19d90036436b1229e430
unknow <138153020+ff6f8d68@users.noreply.github.com>
2024-07-09 16:55:38 +0200

    Update launch.sh

--- linuxMC/library/launch.sh
+++ linuxMC/library/launch.sh
@@ -1,31 +1,156 @@
 #!/bin/bash
+python - << EOF
+#!/usr/bin/env python3
 
-# Directory containing Minecraft and LWJGL files
-MINECRAFT_DIR=./jars/minecraft/$1
-
-username=$2
-
-# Minecraft client JAR
-MINECRAFT_JAR="$MINECRAFT_DIR/client.jar" # Change this to the name of your Minecraft client JAR
-
-# LWJGL JAR
-LWJGL_JAR=./jar/lwjgl/lwjgl.jar # Change this to the name of your LWJGL JAR
-
-# Directory containing LWJGL native libraries
-NATIVES_DIR="$MINECRAFT_DIR/natives"
-
-# Main class of Minecraft (usually net.minecraft.client.main.Main)
-MAIN_CLASS="net.minecraft.client.main.Main"
+import json
+import os
+import platform
+from pathlib import Path
+import subprocess
 
 
+"""
+Debug output
+"""
+def debug(str):
+    if os.getenv('DEBUG') != None:
+        print(str)
+
+"""
+[Gets the natives_string toprepend to the jar if it exists. If there is nothing native specific, returns and empty string]
+"""
+def get_natives_string(lib):
+    arch = ""
+    if platform.architecture()[0] == "64bit":
+        arch = "64"
+    elif platform.architecture()[0] == "32bit":
+        arch = "32"
+    else:
+        raise Exception("Architecture not supported")
+
+    nativesFile=""
+    if not "natives" in lib:
+        return nativesFile
+
+    if "windows" in lib["natives"] and platform.system() == 'Windows':
+        nativesFile = lib["natives"]["windows"].replace("${arch}", arch)
+    elif "osx" in lib["natives"] and platform.system() == 'Darwin':
+        nativesFile = lib["natives"]["osx"].replace("${arch}", arch)
+    elif "linux" in lib["natives"] and platform.system() == "Linux":
+        nativesFile = lib["natives"]["linux"].replace("${arch}", arch)
+    else:
+        raise Exception("Platform not supported")
+
+    return nativesFile
 
 
+"""
+[Parses "rule" subpropery of library object, testing to see if should be included]
+"""
+def should_use_library(lib):
+    def rule_says_yes(rule):
+        useLib = None
 
-# Minecraft arguments (if any)
-MINECRAFT_ARGS="--username $username --gameDir $MINECRAFT_DIR" # You can add more arguments if needed
+        if rule["action"] == "allow":
+            useLib = False
+        elif rule["action"] == "disallow":
+            useLib = True
 
-# Run Minecraft
-java -Xmx2G -Xms1G \
-  -Djava.library.path=$NATIVES_DIR \
-  -cp "$MINECRAFT_JAR:$LWJGL_JAR" \
-  $MAIN_CLASS $MINECRAFT_ARGS
+        if "os" in rule:
+            for key, value in rule["os"].items():
+                os = platform.system()
+                if key == "name":
+                    if value == "windows" and os != 'Windows':
+                        return useLib
+                    elif value == "osx" and os != 'Darwin':
+                        return useLib
+                    elif value == "linux" and os != 'Linux':
+                        return useLib
+                elif key == "arch":
+                    if value == "x86" and platform.architecture()[0] != "32bit":
+                        return useLib
+
+        return not useLib
+
+    if not "rules" in lib:
+        return True
+
+    shouldUseLibrary = False
+    for i in lib["rules"]:
+        if rule_says_yes(i):
+            return True
+
+    return shouldUseLibrary
+
+"""
+[Get string of all libraries to add to java classpath]
+"""
+def get_classpath(lib, mcDir):
+    cp = []
+
+    for i in lib["libraries"]:
+        if not should_use_library(i):
+            continue
+
+        libDomain, libName, libVersion = i["name"].split(":")
+        jarPath = os.path.join(mcDir, "libraries", *
+                               libDomain.split('.'), libName, libVersion)
+
+        native = get_natives_string(i)
+        jarFile = libName + "-" + libVersion + ".jar"
+        if native != "":
+            jarFile = libName + "-" + libVersion + "-" + native + ".jar"
+
+        cp.append(os.path.join(jarPath, jarFile))
+
+    cp.append(os.path.join(mcDir, "versions", lib["id"], f'{lib["id"]}.jar'))
+
+    return os.pathsep.join(cp)
+
+version = '1.16.5'
+username = '{username}'
+uuid = '{uuid}'
+accessToken = '{token}'
+
+mcDir = os.path.join(os.getenv('HOME'), '.minecraft')
+nativesDir = os.path.join(os.getenv('HOME'), 'versions', version, 'natives')
+clientJson = json.loads(
+    Path(os.path.join(mcDir, 'versions', version, f'{version}.json')).read_text())
+classPath = get_classpath(clientJson, mcDir)
+mainClass = clientJson['mainClass']
+versionType = clientJson['type']
+assetIndex = clientJson['assetIndex']['id']
+
+debug(classPath)
+debug(mainClass)
+debug(versionType)
+debug(assetIndex)
+
+subprocess.call([
+    '/usr/bin/java',
+    f'-Djava.library.path={nativesDir}',
+    '-Dminecraft.launcher.brand=custom-launcher',
+    '-Dminecraft.launcher.version=2.1',
+    '-cp',
+    classPath,
+    'net.minecraft.client.main.Main',
+    '--username',
+    username,
+    '--version',
+    version,
+    '--gameDir',
+    mcDir,
+    '--assetsDir',
+    os.path.join(mcDir, 'assets'),
+    '--assetIndex',
+    assetIndex,
+    '--uuid',
+    uuid,
+    '--accessToken',
+    accessToken,
+    '--userType',
+    'mojang',
+    '--versionType',
+    'release'
+])
+EOF
