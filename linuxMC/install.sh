8c800ae1ab3a0500b0921afaa061ee43aa437282
unknow <138153020+ff6f8d68@users.noreply.github.com>
2024-08-25 08:35:49 -0700

    Update install.sh

--- linuxMC/install.sh
+++ linuxMC/install.sh
@@ -2,13 +2,13 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	wget https://linuxmc.vercel.app/pool/linuxMC.sh
 	mkdir library
 	cd library
-	wget https://linuxmc.vercel.app/pool/lib/setupLIB.setup
+	wget https://linuxmc.vercel.app/linuxMC/lib/setupLIB.setup
         sudo bash setupLIB.setup linux
 else
 	#!/bin/bash
-	curl -LJO https://linuxmc.vercel.app/pool/linuxMC.sh
+	curl -LJO https://linuxmc.vercel.app/linuxMC/linuxMC.sh
 	mkdir library
 	cd library
-	curl -LJO https://linuxmc.vercel.app/pool/library/setupLIB.setup
+	curl -LJO https://linuxmc.vercel.app/linuxMC/library/setupLIB.setup
 	bash setupLIB.setup other
 fi
